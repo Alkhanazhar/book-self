b34f0a925300a4338afb4f155df630dbbdbfefa4
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2024-07-03 00:57:31 +0530

    front-end done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 import AuthProvider from "./context/AuthContext.jsx";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://book-self.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
